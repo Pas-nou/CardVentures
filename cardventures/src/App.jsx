a5afdf2437dbe13e40fd923d5c2791372686c4f6
Pas-nou <pascal.renou@live.fr>
2024-05-16 15:16:52 +0200

    ✨ Add global CSS and creation of the homepage

--- cardventures/src/App.jsx
+++ cardventures/src/App.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     <>
-                <Outlet />
-
+      <Outlet />
     </>
   )
 }
