c40987f6758abe60b2e9f7faf9f4dccaac713d69
Pas-nou <pascal.renou@live.fr>
2024-05-16 12:50:13 +0200

    � add databases

---
+++ cardventures/src/data.js
@@ -0,0 +1,294 @@
+export const images = [
+    {
+        id: "1",
+        src: "./src/assets/CAPY1.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Jeux",
+        ],
+    },
+    {
+        id: "2",
+        src: "./src/assets/CAPY2.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Ville",
+        ],
+    },
+    {
+        id: "3",
+        src: "./src/assets/CAPY3.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Forêt",
+        ],
+    },
+    {
+        id: "4",
+        src: "./src/assets/CAPY4.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Mer",
+        ],
+    },
+    {
+        id: "5",
+        src: "./src/assets/CAPY5.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+        ],
+    },
+    {
+        id: "6",
+        src: "./src/assets/CAPY7.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+        ],
+    },
+    {
+        id: "7",
+        src: "./src/assets/CAPY8.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Mer",
+            "Noël"
+        ],
+    },
+    {
+        id: "8",
+        src: "./src/assets/CAT1.webp",
+        theme: [
+            "Chats",
+            "Animaux",
+        ],
+    },
+    {
+        id: "9",
+        src: "./src/assets/CAPY6.webp",
+        theme: [
+            "Capybara",
+            "Animaux",
+            "Forêt",
+            "Tortue",
+        ],
+    },
+    {
+        id: "10",
+        src: "./src/assets/CAT2.webp",
+        theme: [
+            "Chats",
+            "Animaux",
+            "Drôle",
+        ],
+    },
+    {
+        id: "11",
+        src: "./src/assets/CAT3.webp",
+        theme: [
+            "Chats",
+            "Animaux",
+            "Mignon",
+        ],
+    },
+    {
+        id: "12",
+        src: "./src/assets/CHAMPS1.webp",
+        theme: [
+            "Nature",
+            "Fleurs",
+            "Prairie",
+        ],
+    },
+    {
+        id: "13",
+        src: "./src/assets/CHAMPS2.webp",
+        theme: [
+            "Nature",
+            "Fleurs",
+        ],
+    },
+    {
+        id: "14",
+        src: "./src/assets/CHAMPS3.webp",
+        theme: [
+            "Nature",
+            "Fleurs",
+            "Prairie",
+        ],
+    },
+    {
+        id: "15",
+        src: "./src/assets/CHAMPS4.webp",
+        theme: [
+            "Nature",
+            "Prairie",
+        ],
+    },
+    {
+        id: "16",
+        src: "./src/assets/CITY1.webp",
+        theme: [
+            "Ville",
+        ],
+    },
+    {
+        id: "17",
+        src: "./src/assets/DOG1.webp",
+        theme: [
+            "Drôle",
+            "Montagne",
+        ],
+    },
+    {
+        id: "18",
+        src: "./src/assets/FOREST1.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+            "Animaux",
+            "Tortues",
+        ],
+    },
+    {
+        id: "19",
+        src: "./src/assets/FOREST2.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+        ],
+    },
+    {
+        id: "20",
+        src: "./src/assets/FOREST3.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+            "Animaux",
+            "Tortues",
+        ],
+    },
+    {
+        id: "21",
+        src: "./src/assets/FOREST4.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+            "Animaux",
+            "Mignon",
+        ],
+    },
+    {
+        id: "22",
+        src: "./src/assets/FOREST5.webp",
+        theme: [
+            "Nature",
+            "Animaux",
+        ],
+    },
+    {
+        id: "23",
+        src: "./src/assets/FOREST6.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+            "Animaux",
+            "Montagne"
+        ],
+    },
+    {
+        id: "24",
+        src: "./src/assets/FOREST7.webp",
+        theme: [
+            "Forêt",
+            "Nature",
+            "Animaux",
+        ],
+    },
+    {
+        id: "25",
+        src: "./src/assets/MOUNTAIN1.webp",
+        theme: [
+            "Nature",
+            "Fleurs",
+            "Forêt",
+        ],
+    },
+    {
+        id: "26",
+        src: "./src/assets/MOUNTAIN2.webp",
+        theme: [
+            "Montagne",
+        ],
+    },
+    {
+        id: "27",
+        src: "./src/assets/MOUNTAIN3.webp",
+        theme: [
+            "Montagne",
+            "Animaux",
+        ],
+    },
+    {
+        id: "28",
+        src: "./src/assets/MOUNTAIN4.webp",
+        theme: [
+            "Montagne",
+            "Nature"
+        ],
+    },
+    {
+        id: "29",
+        src: "./src/assets/PETS1.webp",
+        theme: [
+            "Nature",
+            "Animaux",
+            "Cringe",
+        ],
+    },
+    {
+        id: "30",
+        src: "./src/assets/PETS2.webp",
+        theme: [
+            "Forêt",
+            "Animaux",
+            "Cringe",
+            "Forêt",
+            "Nature",
+        ],
+    },
+    {
+        id: "31",
+        src: "./src/assets/PETS3.webp",
+        theme: [
+            "Nature",
+            "Forêt",
+            "Drôle",
+            "Animaux",
+        ],
+    },
+    {
+        id: "32",
+        src: "./src/assets/SEA1.webp",
+        theme: [
+            "Ville",
+            "Mer",
+        ],
+    },
+    {
+        id: "33",
+        src: "./src/assets/SEA2.webp",
+        theme: [
+            "Animaux",
+            "Tortues",
+            "Mer",
+        ],
+    },
+
+]
--- cardventures/src/pages/Home.jsx
+++ cardventures/src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import {images} from "../data"
+
+
 function Home
 (){
-    return <h1>coucou home</h1>
+
+    console.log(images)
+    return (
+    <>
+    <h1>coucou home</h1>
+    <img src={images[1].src} alt="" />
+    </>
+    );
 }
 
 export default Home;
