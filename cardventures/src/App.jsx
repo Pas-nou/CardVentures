cac334361459efbf2a10768fb30b7c28d0d7b5d8
Adeline Cornier <adeline.cornier@hotmail.com>
2024-05-16 16:59:06 +0200

    mise en place du context pour le message de la carte

--- cardventures/src/App.jsx
+++ cardventures/src/App.jsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import TextContext from "./context/Context";
+import { useState } from "react";
 
 function App() {
+  const [messageContext, setMessageContext] = useState("ici msg")
+  console.log(messageContext)
   return (
-    <>
+    <TextContext.Provider
+    value={{
+      messageContext: messageContext,
+      setMessageContext: setMessageContext,
+    }}
+    >
       <Outlet />
-    </>
+    </TextContext.Provider>
+
   );
 }
 
---
+++ cardventures/src/context/Context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const TextContext = createContext();
+
+export default TextContext;
+
+
