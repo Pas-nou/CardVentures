64fe76293b35002752787954758860aa5ac219b1
julien44830 <deniaudjuli0@gmail.com>
2024-05-17 15:04:26 +0200

    fin css

--- cardventures/src/pages/Visualisation.jsx
+++ cardventures/src/pages/Visualisation.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Footer";
 import PreviewCard from "../components/PreviewCard";
 
-
 function Visualisation() {
     return (
         <>
-            <PreviewCard />
+            <h2 className="h2-face-arriere ">Voici votre carte postale</h2>
+
+            <section className="visualisation-preview">
+                <PreviewCard />
+            </section>
             <Footer />
         </>
     );
